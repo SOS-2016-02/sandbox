fbb10f2bf1607e3908580614d015be53c7d7af53
crileaech <crileaech@gmail.com>
2016-03-01 11:21:38 +0100

    this will close issue reopen. Closes #5

--- index.js
+++ index.js
@@ -2,3 +2,14 @@
 
 console.log("Hola, cómo estás?");
 console.log("Estoy bien ");
+
+console.log("------------Show me the dataWorkers--------");
+
+var fs = require("fs");
+var data=[];
+var content = fs.readFileSync('dataWorkers.json','utf-8');
+data=JSON.parse(content);
+data.forEach((data)=>{
+	console.log(data.province+ "," +data.year+ "," +data.industry+ "," +data.value);
+});
+console.log("---------------------------------------------");
